15e40ed38353e2d88b1f2f72e591aa01c25613ce
owen gaudio <Owen@Matthews-MacBook-Pro.local>
2021-02-02 12:12:16 -0500

    updated title and desc

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,7 +4,7 @@ const Recipe = props => {
     return <div className="recipe">
         <div className={!props.recipe.complete ? 'recipe-text' : 'recipe-text complete'}>{props.recipe.text}</div>
         <div className="btn-container">
-            <button onClick={() => props.completeRecipe(props.recipe.id)}>{!props.recipe.complete ? 'Edit' : 'Undo'}</button>
+            <button onClick={() => props.completeRecipe(props.recipe.id)}>{!props.recipe.complete ? 'Edit' : 'Done'}</button>
             <button onClick={() => props.deleteRecipe(props.recipe.id)}>X</button>
         </div>
     </div>
