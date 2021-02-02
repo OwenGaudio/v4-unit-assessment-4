fa3c21e039ac442526c651fc71e693d34df3aee1
owen gaudio <Owen@Matthews-MacBook-Pro.local>
2021-02-02 18:12:50 -0500

    finished

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Recipe = props => {
-    return <div className="recipe">
-        <div className={!props.recipe.cooked ? 'recipe-text' : 'recipe-text cooked'}>{props.recipe.text}</div>
+    return <div className={!props.recipe.cooked ? 'recipe' : 'recipe cooked'}>
+        <div className='recipe-text'>{props.recipe.text}</div>
         <div className="btn-container">
             <button onClick={() => props.cookedRecipe(props.recipe.id)}>{!props.recipe.cooked ? 'Cook Meal' : "Haven't made in a while"}</button>
             <button onClick={() => props.deleteRecipe(props.recipe.id)}>Delete</button>
